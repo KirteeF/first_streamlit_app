a23491c78f498ba274765e6b54ca87bf4145593d
KirteeF <143117686+KirteeF@users.noreply.github.com>
2023-08-24 23:18:15 +0530

    Update streamlit_app.py
    added emoji to 1st header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My Mom's New Healthy Dinner")
-streamlit.header('Breakfast Favorites')
+streamlit.header('🍳Breakfast Favorites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard Boiled Free Range Egg')
